7d248a2b1c404601212344bd7ae35601f048c8f6
deesiigneer <xdeesiigneerx@gmail.com>
2022-09-17 00:55:49 +0300

    test autocheck payment

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,8 +1,7 @@
-import nextcord
 from nextcord.ext.commands.bot import Bot
-from nextcord.embeds import Embed
+from nextcord import ButtonStyle, Button, Interaction, Embed, Colour, slash_command, SlashOption
 from nextcord.ext import commands
-from nextcord import Colour, Interaction, slash_command
+from nextcord.ui import View, Select, Modal, Button, button, TextInput
 from nextcord.user import ClientUser
 from os import environ
 from pyspapi import SPAPI
@@ -29,15 +28,50 @@ class GeneralCommands(commands.Cog):
         await interaction.response.send_message('in dev', ephemeral=True)
 
     @slash_command(name='check', description='Проверяет наличие проходки на СПм', guild_ids=guilds)
-    async def check(self, interaction: Interaction, user: nextcord.User = nextcord.SlashOption(
-                                  name='user',
-                                  description='Пользователь в Discord',
-                                  required=False
-                              )):
+    async def check(self, interaction: Interaction, user: nextcord.User = SlashOption(
+        name='user',
+        description='Пользователь в Discord',
+        required=False
+    )):
         if user is not None:
             await interaction.response.send_message(f'{api.get_user(user.id)}', ephemeral=True)
         else:
             await interaction.response.send_message(f'{api.get_user(interaction.user.id)}', ephemeral=True)
+
+    @slash_command(name='donate', description='Проверка оплаты и одновременно пожертвования)', guild_ids=guilds)
+    async def check(self, interaction: Interaction, amount: int = SlashOption(
+        name='amount',
+        description='Кол-во аров для пожертвования',
+        required=True
+    )):
+        try:
+            embed = Embed(title='Donate',
+                          description='Если у этого сообщения, появится реакция от бота, значит оплата прошла успешно!')
+            message = await interaction.send(embed=embed)
+            url = api.payment(amount, 'https://spworlds.ru/', f'{environ.get("WEBHOOK_URL", None)}',
+                              data=f'{interaction.channel_id}-{message.id}')
+            embed.set_footer(text='Для оплаты, нажмите кнопку ниже.')
+            await message.edit(embed=embed, view=DonateButton(url=url))
+        except Exception as e:
+            print(e)
+
+
+class DonateButton(View):
+    def __init__(self, url=None):
+        super().__init__(timeout=None)
+        self.url = url
+        if self.url is not None:
+            self.add_item(
+                Button(style=ButtonStyle.link,
+                       label='Перейти на сайт для оплаты',
+                       url=f'{self.url}'))
+        else:
+            url = 'https://spworlds.ru/'
+            self.add_item(
+                Button(style=ButtonStyle.link,
+                       label='Перейти на сайт для оплаты',
+                       url=f'{url}',
+                       disabled=True))
 
 
 def setup(bot):
--- cogs/hello_message.py
+++ cogs/hello_message.py
@@ -1,10 +1,16 @@
 from aiohttp import web
+from nextcord import Color
 from nextcord.ext import commands, tasks
 from nextcord.ext.commands.bot import Bot
 from os import environ
+from pyspapi import SPAPI
+from json import loads, load
 
 app = web.Application()
 routes = web.RouteTableDef()
+
+api = SPAPI(card_id=environ.get('SP_CARD_ID', None),
+            token=environ.get('SP_TOKEN', None))
 
 
 class Webserver(commands.Cog):
@@ -18,6 +24,26 @@ class Webserver(commands.Cog):
 
         self.webserver_port = int(environ.get('PORT', 8000))
         app.add_routes(routes)
+
+        @routes.post('/webhook/')
+        async def dblwebhook(request):
+            print('Received Webhook...')
+            request_data = await request.read()
+            header = request.headers.get('X-Body-Hash')
+            verified = api.webhook_verify(data=request_data, header=header)
+            if verified:
+                payment = request_data.decode('utf-8').replace("'", '"')
+                payment = loads(payment)
+                webhook_data = str(payment['data'])
+                webhook_data = webhook_data.split("-")
+                channel_id = webhook_data[0]
+                channel = bot.get_channel(int(channel_id))
+                message_id = webhook_data[1]
+                message = await channel.fetch_message(int(message_id))
+                await message.add_reaction('✅')
+            else:
+                return 'Integrity of request compromised...', 401
+            return 200
 
     @tasks.loop()
     async def web_server(self):
